a2138bc3e04b2b8d2129cbbda675be2b08436143
Hazlan Rozaimi <hazlan.rozaimi@heathwallace.com>
2017-05-24 17:52:23 +0800

    ejected from expo

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Text, View, StyleSheet, Button, Alert, ActivityIndicator } from 'react-native';
-import { Constants, ImagePicker } from 'expo';
+import Camera from 'react-native-camera';
 
 export default class App extends React.Component {
     state = {
@@ -21,83 +21,50 @@ export default class App extends React.Component {
     render() {
         return (
             <View style={styles.container}>
-                <Button
-                    title="Press me"
-                    onPress={this._handleButtonPress}
-                />
-
-                <Text style={styles.paragraph}>
-                    Change code in the editor and watch it change on your phone!
-                    Save to get a shareable url. You get a new url each time you save.21
-                </Text>
+                <Camera
+                    ref={(cam) => {
+                        this.camera = cam;
+                    }}
+                    style={styles.preview}
+                    aspect={Camera.constants.Aspect.fill}>
+                    <Text style={styles.capture} onPress={this.takePicture.bind(this)}>[CAPTURE]</Text>
+                </Camera>
             </View>
         );
     }
 
-
-    _maybeRenderUploadingOverlay = () => {
-        if (this.state.uploading) {
-            return (
-                <View style={[StyleSheet.absoluteFill, {backgroundColor: 'rgba(0,0,0,0.4)', alignItems: 'center', justifyContent: 'center'}]}>
-                    <ActivityIndicator
-                        color="#fff"
-                        animating
-                        size="large"
-                    />
-                </View>
-            );
-        }
-    }
-
-    _maybeRenderImage = () => {
-        let { image } = this.state;
-        if (!image) {
-            return;
-        }
-
-        return (
-            <View style={{
-                marginTop: 30,
-                width: 250,
-                borderRadius: 3,
-                elevation: 2,
-                shadowColor: 'rgba(0,0,0,1)',
-                shadowOffset: {width: 4, height: 4},
-                shadowRadius: 5,
-            }}>
-                <View style={{borderTopRightRadius: 3, borderTopLeftRadius: 3, overflow: 'hidden'}}>
-                    <Image
-                        source={{uri: image}}
-                        style={{width: 250, height: 250}}
-                    />
-                </View>
-
-                <Text
-                    onPress={this._copyToClipboard}
-                    onLongPress={this._share}
-                    style={{paddingVertical: 10, paddingHorizontal: 10}}>
-                    {image}
-                </Text>
-            </View>
-        );
+    takePicture() {
+        const options = {};
+        //options.location = ...
+        this.camera.capture({metadata: options})
+            .then( (data) => {
+                console.log(data);
+                // let result = await takeAndUploadPhotoAsync();
+                // console.log('asd'+JSON.stringify( result))
+                //
+                // Alert.alert(
+                //     'Its a '+JSON.parse(result._bodyInit).responses[0].labelAnnotations[0].description+'!'
+                // );
+            })
+            .catch(err => console.error(err));
     }
 }
 
 async function takeAndUploadPhotoAsync() {
     // Display the camera to the user and wait for them to take a photo or to cancel
     // the action
-    let result = await ImagePicker.launchCameraAsync({
-      allowsEditing: true,
-      aspect: [4, 3],
-    });
+    // let result = await ImagePicker.launchCameraAsync({
+    //   allowsEditing: true,
+    //   aspect: [4, 3],
+    // });
 
-    if (result.cancelled) {
-      return;
-    }
+    // if (result.cancelled) {
+    //   return;
+    // }
 
     // ImagePicker saves the taken photo to disk and returns a local URI to it
-    let localUri = result.uri;
-    let filename = localUri.split('/').pop();
+    // let localUri = result.uri;
+    // let filename = localUri.split('/').pop();
 
     // // Infer the type of the image
     // let match = /\.(\w+)$/.exec(filename);
@@ -109,7 +76,7 @@ async function takeAndUploadPhotoAsync() {
     // formData.append('photo', { uri: localUri, name: filename, type });
 
     console.log('sadasdsa')
-    return await fetch('https://vision.googleapis.com/v1/images:annotate?key=', {
+    return await fetch('https://vision.googleapis.com/v1/images:annotate?key=AIzaSyCHiOie9g-r8SrvsgQoA-uddzDlQFSuyHA', {
         method: 'POST',
         body: JSON.stringify({
             "requests": [
@@ -135,7 +102,6 @@ const styles = StyleSheet.create({
         flex: 1,
         alignItems: 'center',
         justifyContent: 'center',
-        paddingTop: Constants.statusBarHeight,
         backgroundColor: '#ecf0f1',
     },
     paragraph: {
